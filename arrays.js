40d7f0efc81bc741a825e3d1a84407828956ac31
Caleb Roberts <calebtakespics@gmail.com>
2021-02-07 20:26:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars  = ["snickers", "hundred grand", "kitkat", "skittles"];
+let chocolateBars  = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 addElementToBeginningOfArray => (array, element){
   array.unshift();
